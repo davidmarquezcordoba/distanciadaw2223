c138e7ff588b80e0fa845646ac69f2e089fecfe0
David Márquez Córdoba <david.marquez.cordoba@gmail.com>
2023-05-28 16:10:59 +0200

    Subida a GitHub
    Versión: 1.0

---
+++ practica-marquez.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ *
+ * @author David Márquez Córdoba
+ * @version 1.0
+ * {@internal Comentario para el desarrollador}
+*/
+
+/**
+ *
+ * Esta función realiza la suma de dos números que se pasan por parámetros y devuelve el resultado de la misma.
+ *
+ * @param int $a El primer número.
+ * @param int $b El segundo número.
+ * @return int La suma de los dos números.
+ *
+ */
+
+function sumar($a, $b) {
+    return $a + $b;
+}
+
+/**
+ *
+ * Esta función realiza la resta de dos números que se pasan por parámetros y devuelve el resultado de la misma.
+ *
+ * @param int $a El primer número.
+ * @param int $b El segundo número.
+ * @return int La resta de los dos números.
+ * 
+ */
+
+function restar($a, $b) {
+    return $a - $b;
+}
+
+
